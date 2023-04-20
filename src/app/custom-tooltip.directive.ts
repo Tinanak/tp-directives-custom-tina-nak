d6c5de462e8a216bfdc8eda036e0f98d8d556bee
am.iris <tinanak459@gmail.com>
2023-04-20 18:27:56 +1100

    fin

--- src/app/custom-tooltip.directive.ts
+++ src/app/custom-tooltip.directive.ts
@@ -1,10 +1,27 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, HostListener, Input, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appCustomTooltip]'
 })
 export class CustomTooltipDirective {
 
-  constructor() { }
+  @Input() tooltip = '';
 
+  constructor(private el : ElementRef, private renderer :Renderer2) { }
+
+
+  ngOnInit(): void {}
+
+
+  @HostListener('mouseenter') onMouseEnter() {
+      this.customTooltipContent();
+  }
+
+  @HostListener('mouseleave') onMouseLeave() {    
+    this.renderer.setStyle(this.el.nativeElement, 'visibility','hidden')
+  }
+
+  private customTooltipContent(){
+      this.renderer.setStyle(this.el.nativeElement, 'visibility','visible')
+  }
 }
